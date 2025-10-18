ddceeea486639faee4bfceeb8a4c9b4ecef53489
EmGuliev2002 <emilguliev2002g@gmail.com>
2025-10-18 17:47:04 +0700

    Вспомогательный класс для создания экземпляров HabitViewModel с его зависимостями.

---
+++ app/src/main/java/ru/routinely/app/viewmodel/HabitViewModelFactory.kt
@@ -0,0 +1,15 @@
+package ru.routinely.app.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import ru.routinely.app.data.HabitRepository
+
+class HabitViewModelFactory(private val repository: HabitRepository) : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        if (modelClass.isAssignableFrom(HabitViewModel::class.java)) {
+            @Suppress("UNCHECKED_CAST")
+            return HabitViewModel(repository) as T
+        }
+        throw IllegalArgumentException("Unknown ViewModel class")
+    }
+}
